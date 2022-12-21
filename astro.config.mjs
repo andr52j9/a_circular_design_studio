d8bbdd3cc468b8b2d1496f873338acd176f144c4
Charlotte Franciska Christiansen <char308@stud.kea.dk>
2022-12-21 08:10:02 +0100

    xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+import sitemap from "@astrojs/sitemap";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://a-circular-design-studio.netlify.app",
+  integrations: [sitemap()],
+});
